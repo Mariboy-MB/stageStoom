a76c3e09e50f067905c1572235d4ab293c2b1819
Mariboy-MB <marianooliveiradasilva@gmail.com>
2024-01-05 02:51:15 -0300

    finishBasicTests

--- cypress/e2e/cart/cart.cy.js
+++ cypress/e2e/cart/cart.cy.js
@@ -1,8 +1,5 @@
 /// <reference types="cypress" />
 
-const selectLogo = () => cy.get('.select-logo');
-const tendaCardOffers = () => cy.get(':nth-child(3) > .hot-link-name');
-
 describe("Add and remove products", () => {
     beforeEach(()=>{
         cy.visit('/');
@@ -11,14 +8,16 @@ describe("Add and remove products", () => {
     it("Tenda product", () => {
         cy.login("66163283414@mailinator.com", "senhaForte123@");
         cy.wait(5000);
-        tendaCardOffers().click();
+        cy.goToTendaCardOffers();
         cy.addProductToCart();
+        cy.removeProductToCart();
     });
 
     it("Other seller products", () => {
         cy.login("66163283414@mailinator.com", "senhaForte123@");
         cy.wait(5000);
-        selectLogo().click();
+        cy.goToSelectProductsOffers();
         cy.addProductToCart();
+        cy.removeProductToCart();
     });
 });
